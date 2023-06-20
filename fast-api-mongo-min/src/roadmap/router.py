c3790fb17bad140d0420ffe2e94517d4d3fbde5b
Yan Pan <yan@yan.fi>
2023-06-20 09:29:22 +0300

    docker build and push actions

--- fast-api-mongo-min/src/roadmap/router.py
+++ fast-api-mongo-min/src/roadmap/router.py
@@ -8,7 +8,7 @@ router = APIRouter()
 
 
 @router.get("/",)
-async def index(request:Request):
+async def index(request: Request):
     """
     rendered roadmap view, modifiers: [[delimiter]], {{VueJS}}
     """
