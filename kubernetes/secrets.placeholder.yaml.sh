ca1fa2ca62b5ea3d6eb5a338b9e3373ff0dd684f
Yan Pan <yan@yan.fi>
2023-06-20 13:41:55 +0300

    kubernetes manifest tested

---
+++ kubernetes/secrets.placeholder.yaml.sh
@@ -0,0 +1,26 @@
+# This file is left blank purposefully.
+# Secrets will only be available on local dev and/or via KeyVault
+
+# to load env file, a script might be helpful
+
+#!/bin/bash
+# k8cmd="kubectl create secret generic secrets-001"
+
+# while read line; do
+#     # Skip empty lines
+#     if [[ -z $line ]]; then
+#         continue
+#     fi
+
+#     name=$(echo $line | cut -d'=' -f1)
+#     value=$(echo $line | cut -d'=' -f2-)
+
+#     # Check name and value are not empty
+#     if [[ -z $name || -z $value ]]; then
+#         continue
+#     fi
+
+#     k8cmd="$k8cmd\\n--from-literal=$name=$value"
+# done < env.env
+
+# echo $k8cmd
