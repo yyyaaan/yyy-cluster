3816cea680ed3e5401903ea9323137e57e65d0fa
Yan Pan <yan@yan.fi>
2023-06-22 16:04:54 +0300

    oauth ready

--- fast-api-mongo-min/src/auth/router.py
+++ fast-api-mongo-min/src/auth/router.py
@@ -4,49 +4,50 @@ from fastapi.security import OAuth2PasswordRequestForm
 from typing import Annotated
 
 from auth import schemas
-from auth.JwtAuth import JwtAuth
+from auth import OAuth
 
 
 router = APIRouter()
-router_protected = APIRouter()
+router_admin = APIRouter()
+typing_auth_user = Annotated[str, Depends(OAuth.is_authenticated_user)]
+typing_auth_admin = Annotated[str, Depends(OAuth.is_authenticated_admin)]
 
-@router_protected.get("/list-users")
-async def list_registered_users(request: Request):
+
+@router_admin.get("/list-users")
+async def list_registered_users(request: Request, username: typing_auth_admin):
     """
     List registered users from database
     """
-    doc = await request.app.collection_user.find().to_list(length=None)
-    return doc
+    docs = await OAuth.list_users()
+    return docs
 
 
-@router_protected.get("/user/{username}", response_model=schemas.User)
+@router_admin.get("/user/me", response_model=schemas.User)
+async def check_bearer_token(request: Request, username: typing_auth_user):
+    """
+    Check the active user that sends a Bearer token
+    """
+    user_doc = await OAuth.get_user(username)
+    return user_doc
+
+
+@router_admin.get("/user/{username}", response_model=schemas.User)
 async def describe_user(request: Request, username: str):
     """
     Get user from database WITHOUT authentication
     """
-    doc = await request.app.collection_user.find_one({"username": username})
+    doc = await OAuth.get_user(username)
     if doc is None:
         raise HTTPException(status_code=404, detail="User not found")
     return doc
 
 
-@router_protected.get("/who-am-I", response_model=schemas.User)
-async def check_bearer_token(request: Request, token: Annotated[str, Depends(JwtAuth.oauth2_scheme)]):
-    """
-    Determine the active user that sends a Bearer token
-    """
-    doc = await JwtAuth(request.app.collection_user).get_current_user(token)
-    if doc is None:
-        raise HTTPException(status_code=401, detail="Invalid token or user disabled")
-    return doc
-
-
 @router.post("/register", status_code=201)
-async def register_new_user(request: Request, user: schemas.UserWithPassword):
+async def register_new_user(user: schemas.UserWithPassword):
     """
     Register a new user
     """
-    res = await JwtAuth(request.app.collection_user).create_user(user)
+    res = await OAuth.create_user(user)
     return {"user_id": str(res.inserted_id)}
 
 
@@ -55,8 +56,7 @@ async def login_for_access_token(
     request: Request, 
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
 ):
-    token_data = await JwtAuth(request.app.collection_user) \
-        .authenticate_user_and_create_token(form_data.username, form_data.password)
+    token_data = await OAuth.authenticate_user_and_create_token(form_data.username, form_data.password)
     
     if not token_data:
         raise HTTPException(
@@ -66,14 +66,6 @@ async def login_for_access_token(
         )
     return token_data
     
-    
-
-# @router.get("/users/me/", response_model=User)
-# async def read_users_me(
-#     current_user: Annotated[User, Depends(get_current_active_user)]
-# ):
-#     return current_user
-
 
 # @router.get("/users/me/items/")
 # async def read_own_items(
--- fast-api-mongo-min/src/main.py
+++ fast-api-mongo-min/src/main.py
@@ -3,10 +3,12 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from settings.settings import Settings
 
 from roadmap.router import router as router_roadmap
-from auth.router import router as router_auth, router_protected
+from auth.router import router as router_auth, router_admin
+
 
 app = FastAPI()
 settings = Settings()
+
 
 
 @app.on_event("startup")
@@ -15,7 +17,6 @@ async def startup_db_client():
     app.mongodb_client = AsyncIOMotorClient(settings.MONGO_URL)
     app.mongodb = app.mongodb_client[settings.MONGO_DB_NAME]
     app.collection_roadmap = app.mongodb["roadmaps"]
-    app.collection_user = app.mongodb["users"]
 
 
 @app.on_event("shutdown")
@@ -26,4 +27,4 @@ async def shutdown_db_client():
 
 app.include_router(router_auth, tags=["Auth"], prefix="/auth")
 app.include_router(router_roadmap, tags=["Roadmap"], prefix="/roadmap")
-app.include_router(router_protected, tags=["Auth-Protected"], prefix="/protected")
+app.include_router(router_admin, tags=["User Admin"], prefix="/admin")
